5571883c57ec25923d30a0eb3077497b0f1091e3
TaesungKim <taesung.kim@posicube.com>
2021-11-10 12:10:26 +0900

    add stack source file

---
+++ stack/stack.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "llist.h"
+#include "stack.h"
+
+s_stack_data* make_stack(void)
+{
+    s_stack_data *p;
+    p = (s_stack_data*)malloc(sizeof(s_stack_data));
+    p->root = (s_llist*)NULL;
+    return p;
+}
+void init_stack(s_stack_data* p)
+{ 
+    if(p == NULL)
+    {
+        return;
+    }
+    return;
+}
+
+void* pop_stack(s_stack_data* p)
+{
+    if(p == NULL)
+    {
+        return NULL;
+    }
+    void* object = get_llist_object(p->root);
+    p->root = rm_llist(p->root);
+    return object;
+}
+
+void push_stack(s_stack_data* p, void* object)
+{
+    if(p == NULL)
+    {
+        return;
+    }
+    if(p->root == NULL)
+    {
+        p->root = open_llist();
+    }
+    else
+    {
+        p->root = mk_llist(p->root, LLIST_DIR_LEFT);
+    }
+    set_llist_object(p, object);
+}
---
+++ stack/stack.h
@@ -0,0 +1,15 @@
+#ifndef _STACK_H_
+#define _STACK_H_
+#include "llist.h"
+
+struct stack_data
+{
+    s_llist* root;
+};
+typedef struct stack_data s_stack_data;
+
+s_stack_data* make_stack(void);
+void init_stack(s_stack_data* root);
+void* pop_stack(s_stack_data* p);
+void push_stack(s_stack_data* p, void* object);
+#endif
