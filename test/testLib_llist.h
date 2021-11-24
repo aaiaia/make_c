45cfabdbbfdaad9d69792eedc2f538432875ff82
PleYa <aaiaia.gg@gmail.com>
2021-11-24 13:02:46 +0900

    add test lib to use commonly

---
+++ test/testLib_llist.h
@@ -0,0 +1,6 @@
+#ifndef _TEST_LLIST_LIB_
+#define _TEST_LLIST_LIB_
+void _PRINT_LLIST_S(s_llist* _s);
+void _PRINT_LLIST_HEAD(s_llist* _s);
+void _PRINT_LLIST_LIST(const char* str, s_llist* _s);
+#endif
