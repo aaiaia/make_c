db3bcaa1d88f9a2d2ed924019012e231ad4938ae
PleYa <aaiaia.gg@gmail.com>
2021-11-27 14:51:05 +0900

    fix funtion name in test_llist.c, cause to using common lib.

--- test/test_llist.c
+++ test/test_llist.c
@@ -2,49 +2,7 @@
 #include <stdlib.h>
 #include "../llist.h"
 
-void _PRINT_S(s_llist* _s)
-{
-	{
-		if(_s!=NULL)	printf("prev=0x%08x, loc=0x%08x, next=0x%08x\r\n",_s->prev , _s, _s->next);
-		else		printf("Empty\r\n");
-	}
-}
-
-void _PRINT_HEAD(s_llist* _s)
-{
-	{
-		s_llist* _head;
-		_head=find_llist_end(_s, LLIST_DIR_PREV);
-		printf("head info: ");
-		_PRINT_S(_head);
-	}
-}
-
-void _PRINT_LIST_S(const char* str, s_llist* _s)
-{
-	{
-		s_llist* _head;
-		s_llist* _tmp;
-		_head = find_llist_end(_s, LLIST_DIR_PREV);
-		printf("%s", str);
-		if(_head!=NULL)
-		{
-			_PRINT_S(_head);
-			_tmp=_head->next;
-			while(_tmp!=NULL)
-			{
-				_PRINT_S(_tmp);
-				_tmp=_tmp->next;
-			}
-			printf("\r\n");
-		}
-		else
-		{
-			printf("Empty List\r\n");
-		}
-	}
-}
-			
+#include "llist_test.Lib.h"
 
 int main(int argc, char** argv)
 {
@@ -52,15 +10,15 @@ int main(int argc, char** argv)
 	s_llist* s1=NULL;
 	s_llist* sn=NULL;
 
-	_PRINT_S(s0);
+	_PRINT_LLIST_S(s0);
 	/* Open llist start */
 	s0=open_llist();
 	/* Open llist end */
-	_PRINT_S(s0);
+	_PRINT_LLIST_S(s0);
 
 	printf("add llist next @ s0 ");
-	_PRINT_S(s0);
-	_PRINT_LIST_S("[START LIST]\r\n", s0);
+	_PRINT_LLIST_S(s0);
+	_PRINT_LLIST_LIST("[START LIST]\r\n", s0);
 	for(int i=0; i<11; i++)
 	{
 		if((s0=mk_llist(s0, LLIST_DIR_NEXT)->prev)==NULL)
@@ -68,15 +26,15 @@ int main(int argc, char** argv)
 			printf("has err, add llist next\r\n");
 		}
 		/*check origin of llist*/
-		_PRINT_HEAD(s0);
+		_PRINT_LLIST_HEAD(s0);
 
-		_PRINT_LIST_S("",s0);
+		_PRINT_LLIST_LIST("",s0);
 	}
 
 	printf("add llist prev @ s0 ");
-	_PRINT_S(s0);
+	_PRINT_LLIST_S(s0);
 	printf("START LIST\r\n");
-	_PRINT_LIST_S("[START LIST]\r\n", s0);
+	_PRINT_LLIST_LIST("[START LIST]\r\n", s0);
 	for(int i=0; i<17; i++)
 	{
 		if((s0=mk_llist(s0, LLIST_DIR_PREV))==NULL)
@@ -84,15 +42,15 @@ int main(int argc, char** argv)
 			printf("has err, add llist prev\r\n");
 		}
 		/*check origin of llist*/
-		_PRINT_HEAD(s0);
+		_PRINT_LLIST_HEAD(s0);
 
-		_PRINT_LIST_S("",s0);
+		_PRINT_LLIST_LIST("",s0);
 	}
 
 	s1=s0->next;
 	printf("add llist prev @ s1 ");
-	_PRINT_S(s1);
-	_PRINT_LIST_S("[START LIST]\r\n", s1);
+	_PRINT_LLIST_S(s1);
+	_PRINT_LLIST_LIST("[START LIST]\r\n", s1);
 	for(int i=0; i<13; i++)
 	{
 		if((s1=mk_llist(s1, LLIST_DIR_PREV))==NULL)
@@ -100,16 +58,16 @@ int main(int argc, char** argv)
 			printf("has err, add llist prev\r\n");
 		}
 		/*check origin of llist*/
-		_PRINT_HEAD(s0);
+		_PRINT_LLIST_HEAD(s0);
 
-		_PRINT_LIST_S("",s0);
+		_PRINT_LLIST_LIST("",s0);
 	}
 
 
 	s1=s0->next;
 	printf("rm_llist @ s1 ");
-	_PRINT_S(s1);
-	_PRINT_LIST_S("[START LIST]\r\n", s1);
+	_PRINT_LLIST_S(s1);
+	_PRINT_LLIST_LIST("[START LIST]\r\n", s1);
 	for(int i=0; i<17; i++)
 	{
 		if((s1=rm_llist(s1))==NULL)
@@ -117,12 +75,12 @@ int main(int argc, char** argv)
 			printf("has err, sel llist\r\n");
 		}
 
-		_PRINT_LIST_S("rm_llist @s1\r\n",s0);
+		_PRINT_LLIST_LIST("rm_llist @s1\r\n",s0);
 	}
 
 	printf("rm_llist @ s0 ");
-	_PRINT_S(s0);
-	_PRINT_LIST_S("[START LIST]\r\n", s0);
+	_PRINT_LLIST_S(s0);
+	_PRINT_LLIST_LIST("[START LIST]\r\n", s0);
 	for(int i=0; i<7; i++)
 	{
 		if((s0=rm_llist(s0))==NULL)
@@ -130,13 +88,13 @@ int main(int argc, char** argv)
 			printf("has err, sel llist\r\n");
 		}
 
-		_PRINT_LIST_S("rm_llist @s0\r\n",s0);
+		_PRINT_LLIST_LIST("rm_llist @s0\r\n",s0);
 	}
 
 	sn=find_llist_end(s0,LLIST_DIR_NEXT);
 	printf("rm_llist @ sn ");
-	_PRINT_S(sn);
-	_PRINT_LIST_S("[START LIST]\r\n", sn);
+	_PRINT_LLIST_S(sn);
+	_PRINT_LLIST_LIST("[START LIST]\r\n", sn);
 	for(int i=0; i<15; i++)
 	{
 		if((sn=rm_llist(sn))==NULL)
@@ -144,14 +102,14 @@ int main(int argc, char** argv)
 			printf("has err, sel llist\r\n");
 		}
 
-		_PRINT_LIST_S("rm_llist @sn\r\n",sn);
+		_PRINT_LLIST_LIST("rm_llist @sn\r\n",sn);
 	}
 
 	/* Delete All Nodes Start */
 	s1=s0->next;
 	printf("rm_llist @ s1 ");
-	_PRINT_S(s1);
-	_PRINT_LIST_S("[START LIST]\r\n", s1);
+	_PRINT_LLIST_S(s1);
+	_PRINT_LLIST_LIST("[START LIST]\r\n", s1);
 	for(int i=0; s1!=NULL; i++)
 	{
 		if((s1=rm_llist(s1))==NULL)
@@ -159,7 +117,7 @@ int main(int argc, char** argv)
 			printf("has err, sel llist\r\n");
 		}
 
-		_PRINT_LIST_S("rn_llist @s1\r\n",s1);
+		_PRINT_LLIST_LIST("rn_llist @s1\r\n",s1);
 	}
 	/* Delete All Nodes End */
 	/* Clear variable all start */
@@ -172,12 +130,12 @@ int main(int argc, char** argv)
 	s0 = open_llist();
 	/* Open llist end */
 
-	_PRINT_LIST_S("[START LIST]\r\n", s0);
+	_PRINT_LLIST_LIST("[START LIST]\r\n", s0);
 	for(int i=0; i<11; i++)
 	{
 		s0=mk_llist(s0, LLIST_DIR_NEXT);
 		printf("new=0x%08x\r\n", s0);
-		_PRINT_LIST_S("[mk LIST prev @s0]\r\n", s0);
+		_PRINT_LLIST_LIST("[mk LIST prev @s0]\r\n", s0);
 	}
 
 	/* set second nodes */
@@ -185,8 +143,8 @@ int main(int argc, char** argv)
 
 	/* test close llist */
 	s1=close_llist(s1);
-	_PRINT_S(s1);
-	_PRINT_S(s0);
+	_PRINT_LLIST_S(s1);
+	_PRINT_LLIST_S(s0);
 	s0 = NULL;
 	s1 = NULL;
 
