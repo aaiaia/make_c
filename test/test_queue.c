c5f56a835a1d8796e212a0e3e57613d8a392a224
PleYa <aaiaia.gg@gmail.com>
2021-11-27 16:21:35 +0900

    fix put and get queue continuously to test.

--- test/test_queue.c
+++ test/test_queue.c
@@ -41,6 +41,23 @@ int main(int argc, char* argv[])
 	printf("get %u\n", (unsigned int)get_queue(test));
 	_PRINT_LLIST_LIST("[START LIST]\r\n", test->in);
 
+	printf("\n\n");
+
+	_PRINT_LLIST_LIST("[START LIST] put, obj_addr=100~199\r\n", test->in);
+	for(unsigned int i=100; i<200; i++)
+	{
+		put_queue(test, (void*)i);
+	}
+	_PRINT_LLIST_LIST("[END LIST] put, obj_addr=100~199\r\n", test->in);
+
+	printf("\n\n");
+
+	_PRINT_LLIST_LIST("[START LIST] get count 102\r\n", test->in);
+	for(unsigned int i=0; i<102; i++)
+	{
+		printf("get %u\n", (unsigned int)get_queue(test));
+	}
+	_PRINT_LLIST_LIST("[END LIST] get count 102\r\n", test->in);
 
     return 0;
 }
