19f684a6590a5f1bf829da7eaf3c38a88227e36e
PleYa <aaiaia.gg@gmail.com>
2021-12-30 21:07:04 +0900

    fix to work cmd:  make depend

--- lib/IncludePost.mk
+++ lib/IncludePost.mk
@@ -4,43 +4,38 @@ ifeq ($(IS_SHARED),1)
 SHARED_FLAGS = -fPIC
 SHARED_SO_NAME = lib$(LIB_NAME).so.$(MAJOR_VERSION)
 SHARED_REAL_NAME = $(SHARED_SO_NAME).$(MINOR_VERSION)
-LIB_FULL_NAME = $(ROOT_LIB_DIR)/$(OBJS_DIR)/$(SHARED_REAL_NAME)
+LIB_FULL_NAME = $(ROOT_LIB_DIR)/$(SHARED_REAL_NAME)
 else
-LIB_FULL_NAME = $(ROOT_LIB_DIR)/$(OBJS_DIR)/lib$(LIB_NAME).a
+LIB_FULL_NAME = $(ROOT_LIB_DIR)/lib$(LIB_NAME).a
 endif
+$(info LIB_FULL_NAME = ${LIB_FULL_NAME})
  
-LIB_OBJS = $(LIB_SRCS:%.c=$(OBJS_DIR)/%.o)
+#LIB_OBJS = $(LIB_SRCS:%.c=$(OBJS_DIR)/%.o)
+#TEMP = $(subst $(ROOT_SRC_DIR), $(ROOT_OUT_DIR), $(LIB_SRCS))
+#LIB_OBJS = $(TEMP:%.c=%.o)
+LIB_ITEMS = $(patsubst %.c,%,$(subst $(ROOT_SRC_DIR)/, , $(LIB_SRCS)))
+$(info LIB_ITEMS = $(LIB_ITEMS))
+LIB_OBJS = $(patsubst %.c,%.o,$(subst $(ROOT_SRC_DIR), $(OBJS_DIR), $(LIB_SRCS)))
+$(info LIB_OBJS = ${LIB_OBJS})
  
 ALL_LIBS = -l$(LIB_NAME) $(DEPEND_LIBS) $(LIBS)
- 
-TARGET_OBJS = $(TARGET_SRCS:%.c=$(OBJS_DIR)/%.o)
-TARGET_NAMES = $(TARGET_SRCS:%.c=$(OBJS_DIR)/%)
- 
-.SUFFIXES : .c .o
- 
-# IncludePost.mk
- 
-ifeq ($(IS_SHARED),1)
-SHARED_FLAGS = -fPIC
-SHARED_SO_NAME = lib$(LIB_NAME).so.$(MAJOR_VERSION)
-SHARED_REAL_NAME = $(SHARED_SO_NAME).$(MINOR_VERSION)
-LIB_FULL_NAME = $(ROOT_LIB_DIR)/$(OBJS_DIR)/$(SHARED_REAL_NAME)
-else
-LIB_FULL_NAME = $(ROOT_LIB_DIR)/$(OBJS_DIR)/lib$(LIB_NAME).a
-endif
- 
-LIB_OBJS = $(LIB_SRCS:%.c=$(OBJS_DIR)/%.o)
- 
-ALL_LIBS = -l$(LIB_NAME) $(DEPEND_LIBS) $(LIBS)
- 
-TARGET_OBJS = $(TARGET_SRCS:%.c=$(OBJS_DIR)/%.o)
-TARGET_NAMES = $(TARGET_SRCS:%.c=$(OBJS_DIR)/%)
+$(info ALL_LIBS = ${ALL_LIBS})
+
+TARGET_ITEMS = $(patsubst %.c,%,$(subst $(ROOT_TRG_DIR)/, , $(TARGET_SRCS)))
+$(info TARGET_ITEMS = $(TARGET_ITEMS))
+TARGET_OBJS = $(patsubst %.c,%.o,$(subst $(ROOT_TRG_DIR), $(OBJS_DIR), $(TARGET_SRCS)))
+$(info TARGET_OBJS = ${TARGET_OBJS})
+TARGET_NAMES = $(TARGET_ITEMS:%=$(ROOT_OUT_DIR)/%)
+$(info TARGET_NAMES = ${TARGET_NAMES})
  
 .SUFFIXES : .c .o
  
 all : lib subdirs targets
  
 subdirs : 
+	@echo "==================================================="
+	@echo "subdirs"
+	@echo "==================================================="
 	@for dir in $(SUB_DIRS); do \
 		$(MAKE) -C $$dir all; \
 		if [ $$? != 0 ]; then exit 1; fi; \
@@ -49,6 +44,9 @@ subdirs :
 lib : $(LIB_FULL_NAME)
  
 liball : $(LIB_FULL_NAME) 
+	@echo "==================================================="
+	@echo "liball"
+	@echo "==================================================="
 	@for dir in $(SUB_DIRS); do \
 		$(MAKE) -C $$dir liball; \
 		if [ $$? != 0 ]; then exit 1; fi; \
@@ -57,6 +55,9 @@ liball : $(LIB_FULL_NAME)
 targets : $(TARGET_NAMES)
  
 $(LIB_FULL_NAME) : $(LIB_OBJS)
+	@echo "==================================================="
+	@echo "LIB_FULL_NAME"
+	@echo "==================================================="
 	@`[ -d $(ROOT_LIB_DIR)/$(OBJS_DIR) ] || $(MKDIR) -p $(ROOT_LIB_DIR)/$(OBJS_DIR)`
 ifeq ($(IS_SHARED),1)
 	$(CC) -shared -Wl,-soname,$(SHARED_SO_NAME) -o $@ $(LIB_OBJS)
@@ -89,14 +90,19 @@ else
 endif
  
 depend :
-	@`[ -d $(OBJS_DIR) ] || $(MKDIR) $(OBJS_DIR)`
+	@`[ -d $(OBJS_DIR) ] || $(MKDIR) -p $(OBJS_DIR)`
 	@$(RM) -f $(DEPEND_FILE)
-	@for FILE in $(LIB_SRCS:%.c=%); do \
-		$(CC) -MM -MT $(OBJS_DIR)/$$FILE.o $$FILE.c $(CFLAGS) $(DBG_FLAGS) $(SHARED_FLAGS) $(INC_DIRS) >> $(DEPEND_FILE); \
+	@echo "Abstract Library Source Dependency"
+	@for ITEM in $(LIB_ITEMS); do \
+        echo "item: $$ITEM, src: $(ROOT_SRC_DIR)/$$ITEM.c, obj: $(OBJS_DIR)/$$ITEM.o"; \
+		$(CC) -MM -MT $(OBJS_DIR)/$$ITEM.o $(ROOT_SRC_DIR)/$$ITEM.c $(CFLAGS) $(DBG_FLAGS) $(SHARED_FLAGS) $(INC_DIRS) >> $(DEPEND_FILE); \
 	done
-	@for FILE in $(TARGET_SRCS:%.c=%); do \
-		$(CC) -MM -MT $(OBJS_DIR)/$$FILE.o $$FILE.c $(CFLAGS) $(DBG_FLAGS) $(INC_DIRS) >> $(DEPEND_FILE); \
+	@echo "Abstract Target Source Dependency"
+	@for ITEM in $(TARGET_ITEMS); do \
+        echo "item: $$ITEM, src: $(PROJ_ROOT)/$$ITEM.c, obj: $(OBJS_DIR)/$$ITEM.o"; \
+		$(CC) -MM -MT $(OBJS_DIR)/$$ITEM.o $(ROOT_TRG_DIR)/$$ITEM.c $(CFLAGS) $(DBG_FLAGS) $(INC_DIRS) >> $(DEPEND_FILE); \
 	done
+	@echo "DEPEND_FILE: $(DEPEND_FILE)"
  
 dependall : depend
 	@for dir in $(SUB_DIRS); do \
--- lib/IncludePre.mk
+++ lib/IncludePre.mk
@@ -9,21 +9,42 @@ MKDIR = mkdir
 MAKE = make
 LN = ln
 STRIP = strip
- 
-ROOT_SRC_DIR = $(PROJ_ROOT)/Source
-ROOT_OUT_DIR = $(PROJ_ROOT)/Out
-ROOT_LIB_DIR = $(ROOT_OUT_DIR)/Library
- 
-INC_DIRS = -I$(PROJ_ROOT)/Include
+
+SRC_DIR_NAME = src
+INC_DIR_NAME = include
+
+TRG_DIR_NAME = app
+
+OUT_DIR_NAME = out
+OUT_LIB_DIR_NAME = lib
+# Set Path
+ROOT_SRC_DIR = $(PROJ_ROOT)/$(SRC_DIR_NAME)
+$(info  ROOT_SRC_DIR = ${ROOT_SRC_DIR})
+INC_DIRS = -I$(PROJ_ROOT)/$(INC_DIR_NAME)
+$(info INC_DIRS  = ${INC_DIRS})
+
+ROOT_TRG_DIR = $(PROJ_ROOT)/$(TRG_DIR_NAME)
+$(info ROOT_TRG_DIR  = ${ROOT_TRG_DIR})
+
+ROOT_OUT_DIR = $(PROJ_ROOT)/$(OUT_DIR_NAME)
+$(info ROOT_OUT_DIR = ${ROOT_OUT_DIR})
+ROOT_LIB_DIR = $(ROOT_OUT_DIR)/$(OUT_LIB_DIR_NAME)
+$(info ROOT_LIB_DIR = ${ROOT_LIB_DIR})
  
 ifeq ($(RELEASE), 1)
-OBJS_DIR = Release
+OBJS_DIR_NAME = release
 DBG_FLAGS = -O2 -DNDEBUG
 else
-OBJS_DIR = Debug
+OBJS_DIR_NAME = debug
 DBG_FLAGS = -g -O0 -DDEBUG
 endif
+$(info OBJS_DIR_NAME = ${OBJS_DIR_NAME})
+
+OBJS_DIR = $(PROJ_ROOT)/$(OBJS_DIR_NAME)
+$(info OBJS_DIR = ${OBJS_DIR})
  
 DEPEND_FILE = $(OBJS_DIR)/depend_file
+$(info DEPEND_FILE = ${DEPEND_FILE})
  
-LIB_DIRS = -L$(ROOT_LIB_DIR)/$(OBJS_DIR)
+LIB_DIRS = -L$(ROOT_LIB_DIR)/$(OBJS_DIR_NAME)
+$(info LIB_DIRS = ${LIB_DIRS})
